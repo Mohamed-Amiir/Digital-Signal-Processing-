2feca8529adf01d50bf0d64edb5ed761e66c917e
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-12-16 19:42:28 +0200

    TestCase 1 DONE

--- Task9/task9.py
+++ Task9/task9.py
@@ -1,15 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import firwin, lfilter
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from scipy.signal import freqz
-
+import CompareSignal as test
 class FIRFilterApp:
     def __init__(self, master):
         self.master = master
         self.master.title("FIR Filter Application")
-
         # Initialize variables
         self.filter_type_var = tk.StringVar(value='Low pass')
         self.fs_var = tk.DoubleVar(value=0)
@@ -17,10 +14,8 @@ class FIRFilterApp:
         self.fc_var = tk.DoubleVar(value=0)
         self.transition_band_var = tk.DoubleVar(value=0)
         self.lp_coefficients = None
-
         # Create GUI elements
         self.create_widgets()
-
     def create_widgets(self):
         tk.Label(self.master, text="Filter Type:").grid(row=0, column=0, padx=10, pady=5)
         filter_types = ['Low pass', 'High pass', 'Band pass', 'Band stop']
@@ -42,7 +37,7 @@ class FIRFilterApp:
         tk.Label(self.master, text="TW:").grid(row=4, column=0, padx=10, pady=5)
         self.transition_band_entry = tk.Entry(self.master, textvariable=self.transition_band_var, width=50)
         self.transition_band_entry.grid(row=4, column=1, padx=10, pady=5)
-          # Button to load values from file
+        # Button to load values from file
         load_button = tk.Button(self.master, text="Load from File", command=self.load_values_from_file)
         load_button.grid(row=5, column=0, columnspan=2, pady=10)
 
@@ -65,8 +60,7 @@ class FIRFilterApp:
                     elif key == 'fc':
                         self.fc_var.set(float(value))
                     elif key == 'transitionband':
-                        self.transition_band_var.set(float(value))    
-    
+                        self.transition_band_var.set(float(value))       
     def calculate_LowPass_HD(self,FCnorm, N):
         result = []
         for n in range(int(-(N // 2)), int((N // 2) + 1)):
@@ -110,29 +104,30 @@ class FIRFilterApp:
                 x = (2 * F1 * ((np.sin(n * 2 * np.pi * F1)) / (n * 2 * np.pi * F1)))+(-2 * F2 * ((np.sin(n * 2 * np.pi * F2)) / (n * 2 * np.pi * F2)))
             result.append(x)    
         
-        return result 
-    
+        return result    
     def calculate_Hamming(self, N):
         result = []
         for n in range(int(-(N // 2)), int((N // 2) + 1)):  
             x = 0.54 + 0.46 * np.cos((2 * np.pi * n) / N)
             result.append(x)    
-        return result 
-        
+        return result       
     def calculte_Haning(self,N):
         result = []
         for n in range(int(-(N // 2)), int((N // 2) + 1)):  
             x = 0.5 + 0.5 * np.cos((2 * np.pi * n) / N)
             result.append(x)    
         return result 
-
     def FIR(self,filter,window,N, FCnorm):
         F = []
         W = []
+        output_file = ""
         if(filter == "Low pass"):
             F = self.calculate_LowPass_HD(FCnorm,N)
+            output_file = "C:\\Users\\lenovo\\Desktop\\My-Github\\DSP\\Task9\\FIR test cases\\Testcase 1\\LPFCoefficients.txt"
         elif(filter == "High pass"):
-            F = self.calculate_HighPass_HD(FCnorm,N)   
+            F = self.calculate_HighPass_HD(FCnorm,N) 
+            output_file = "C:\\Users\\lenovo\\Desktop\\My-Github\\DSP\\Task9\\FIR test cases\\Testcase 3\\HPFCoefficients.txt"
+
         # elif(filter == "bandpass"):
         #     F = self.calculate_BandPass_HD(FCnorm,N)   
         # elif(filter == "lowpass"):
@@ -154,8 +149,7 @@ class FIRFilterApp:
 
         for n in range(int(-(N // 2)), int((N // 2) + 1)):
             indices.append(n)
-        return H, indices
-
+        return H, indices,output_file
     def run_fir_filter(self):
         filter_type = self.filter_type_var.get()
         fs = float(self.fs_var.get())/1000
@@ -193,16 +187,12 @@ class FIRFilterApp:
                 N = int(np.ceil(3.3 / deltaF))
             FCnormalized = (fc / fs) + (deltaF / 2)
     
-        result, resultIndices = self.FIR(filter_type,window,N, FCnormalized)
+        result, resultIndices,outputFile = self.FIR(filter_type,window,N, FCnormalized)
+
+        test.Compare_Signals(outputFile,resultIndices,result)
+
         self.plot_results(resultIndices, result)
-        self.save_coefficients(resultIndices,result)
-    
-    def compute_num_taps(self, delta_f, stop_attenuation, fs):
-        delta_omega = 2 * np.pi * delta_f / fs
-        num_taps = int(6.6 * fs / delta_omega)
-        if num_taps % 2 == 0:  # Ensure it is odd
-            num_taps += 1
-        return num_taps
+        self.save_coefficients(resultIndices,result)  
     def plot_results(self, indices, res):
         plt.plot(indices, res)
         plt.title('FIR Lowpass Filter Frequency Response')
@@ -221,12 +211,9 @@ class FIRFilterApp:
             data_to_save = np.column_stack((indecis, coefficients))
             np.savetxt(file_path, data_to_save, fmt='%d %.10f', delimiter=' ', newline='\n')
             messagebox.showinfo("Saved", "Data saved to {}".format(file_path))
-
-
 def main():
     root = tk.Tk()
     app = FIRFilterApp(root)
     root.mainloop()
-
 if __name__ == "__main__":
     main()
